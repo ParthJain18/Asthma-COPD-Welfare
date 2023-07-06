1601f5facea9a32c9716c45cdef902ecfd1d3eff
Parth Jain <parthjain1812@gmail.com>
2023-07-06 14:08:34 +0530

    -

--- COPDAsthma/app/src/main/java/com/example/copd_asthma/MainActivity.kt
+++ COPDAsthma/app/src/main/java/com/example/copd_asthma/MainActivity.kt
@@ -3,13 +3,39 @@ package com.example.copd_asthma
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.copd_asthma.ui.theme.COPDAsthmaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,17 +56,267 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "StartScreen") {
+        composable("StartScreen") {
+            StartScreen(
+                onLogIn = { navController.navigate("HomeScreen") },
+                onSignUp = {navController.navigate("SignUpScreen")})
+        }
+        composable("SignUpScreen") {
+            SignUpScreen (onNavigate = { navController.navigate("HomeScreen") } )
+        }
+        composable("HomeScreen") {
+            HomeScreen()
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StartScreen(modifier: Modifier = Modifier, onLogIn: () -> Unit, onSignUp: () -> Unit) {
+
+    var userName by remember { mutableStateOf("") }
+    var userPass by remember { mutableStateOf("") }
+
+
+
+
+    Column(
+
+        modifier.padding(all = 10.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+
+        ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo2),
+            contentDescription ="header",
+            modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(300.dp)
+        )
+
+
+        Text(
+            text = "Account Details",
+            modifier
+                .padding(start = 15.dp, end = 10.dp, top = 10.dp)
+                .fillMaxWidth(),
+            fontSize = 21.sp,
+            textAlign = TextAlign.Center
+        )
+        TextField(
+            value = userName,
+            onValueChange = { userName = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("User Name")}
+        )
+        TextField(
+            value = userPass,
+            onValueChange = { userPass = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("Password")}
+        )
+
+        Button(
+            onClick = onLogIn,
+            modifier.padding(top = 30.dp).width(200.dp),
+            colors = ButtonDefaults.buttonColors(Color(80, 160, 96))
+        ) {
+            Text(text = "Log In",
+                modifier.padding(horizontal = 10.dp, vertical = 5.dp),
+                fontSize = 20.sp
+            )
+        }
+        Text(
+            text = "OR",
+            modifier
+                .padding(start = 15.dp, end = 10.dp, top = 10.dp)
+                .fillMaxWidth(),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Don't have an account?",
+            modifier
+                .padding(start = 15.dp, end = 10.dp, top = 10.dp)
+                .fillMaxWidth(),
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Button(
+            onClick = onSignUp,
+            modifier.padding(top = 30.dp).width(200.dp),
+            colors = ButtonDefaults.buttonColors(Color(80, 160, 96))
+        ) {
+            Text(text = "Sign up",
+                modifier.padding(horizontal = 10.dp, vertical = 5.dp),
+                fontSize = 20.sp
+            )
+        }
+
+
+
+
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen(modifier: Modifier = Modifier, onNavigate: () -> Unit) {
+
+    var userName by remember { mutableStateOf("") }
+    var userPass by remember { mutableStateOf("") }
+    var age by remember { mutableStateOf("") }
+    var packHistory by remember { mutableStateOf("") }
+    var fev1 by remember { mutableStateOf("") }
+    var fvc by remember { mutableStateOf("") }
+    var mwt1 by remember { mutableStateOf("") }
+    var mwt2 by remember { mutableStateOf("") }
+    var mwtBest by remember { mutableStateOf("") }
+
+
+    Column(
+
+        modifier.padding(all = 10.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+
+        ) {
+
+
+
+        Text(
+            text = "Please Enter Your Spirometry Results:",
+            modifier
+                .padding(start = 15.dp, end = 10.dp, top = 10.dp)
+                .fillMaxWidth(),
+            fontSize = 21.sp,
+            textAlign = TextAlign.Center
+        )
+        TextField(
+            value = userName,
+            onValueChange = { userName = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("User Name")}
+        )
+        TextField(
+            value = userPass,
+            onValueChange = { userPass = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("Password")}
+        )
+        TextField(
+            value = age,
+            onValueChange = { age = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("Age")}
+        )
+        TextField(
+            value = packHistory,
+            onValueChange = { packHistory = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("Cigarette Pack History")}
+        )
+
+        TextField(
+            value = fev1,
+            onValueChange = { fev1 = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("FEV 1")}
+        )
+        TextField(
+            value = fvc,
+            onValueChange = { fvc = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("FVC")}
+        )
+        TextField(
+            value = mwt1,
+            onValueChange = { mwt1 = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("MWT 1")}
+        )
+        TextField(
+            value = mwt2,
+            onValueChange = { mwt2 = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("MWT2")}
+        )
+        TextField(
+            value = mwtBest,
+            onValueChange = { mwtBest = it },
+            modifier
+                .padding(all = 10.dp)
+                .fillMaxWidth(),
+            maxLines = 1,
+            label = {Text("MWT Best")}
+        )
+
+
+
+        Button(
+            onClick = onNavigate,
+            modifier.padding(top = 30.dp).width(200.dp),
+            colors = ButtonDefaults.buttonColors(Color(80, 160, 96))
+        ) {
+            Text(text = "Sign up",
+                modifier.padding(horizontal = 10.dp, vertical = 5.dp),
+                fontSize = 20.sp
+            )
+        }
+
+
+    }
+
+}
+
+@Composable
+fun HomeScreen() {
+    Text ("In Progress")
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    COPDAsthmaTheme {
-        Greeting("Android")
-    }
+fun Preview() {
+    SignUpScreen {}
 }
