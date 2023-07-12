94fbeb4c68bbd11d84bed752269cb93fcf68382a
Ninad Chaware <97106629+ninadch06@users.noreply.github.com>
2023-07-12 13:42:12 +0530

    Add files via upload

--- HomeScreen.kt
+++ HomeScreen.kt
@@ -3,149 +3,159 @@ package com.example.copd_asthma.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.material.ScaffoldState
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.copd_asthma.NavDrawerItem
+
 import com.parse.ParseUser
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
 
 val currentUser = ParseUser.getCurrentUser()
 
 val name = currentUser.get("name")
 val sever = currentUser.get("severity")
 
-@OptIn(ExperimentalMaterial3Api::class)
+@ExperimentalMaterial3Api
 @Composable
 fun HomeScreen() {
-
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
     Scaffold(
         modifier = Modifier,
-        topBar =  {
+        topBar = {
             TopAppBar(
                 title = {
                     Text(text = "Jetpack Compose")
                 },
                 navigationIcon = {
-                    IconButton(onClick = { })
+                    IconButton(onClick = { scope.launch { drawerState.open() }})
                     { Icon(imageVector = Icons.Rounded.Menu, contentDescription = "Drawer Icon") }
                 },
                 actions = {
-                    IconButton(onClick = { })
+                    IconButton(onClick = {
+
+
+                    })
                     {
                         Icon(
                             imageVector = Icons.Rounded.Search,
                             contentDescription = "",
-                            tint = Color.White
+                            tint = Color.Green
                         )
                     }
                 }
             )
         }
-    ) { contentPadding ->
-       Column() {
 
-
-        Text(text = "Hello, "+name,
-
-            modifier = Modifier
-                .padding(contentPadding)
-                .fillMaxWidth(),
-            fontSize = 35.sp,
-            textAlign = TextAlign.Center)
-
-        Text(text = "Your COPD Severity Level is:",
-            modifier = Modifier
-                .padding(contentPadding)
-                .fillMaxWidth(),
-            fontSize = 25.sp,
-            textAlign = TextAlign.Center)
-
-        Text(text = ""+ sever,
-            modifier = Modifier
-                .padding(contentPadding)
-                .fillMaxWidth(),
-            fontSize = 35.sp,
-            textAlign = TextAlign.Center)
-    }
-       }
-}
-@Composable
-
-fun Drawer(scope: CoroutineScope ,scaffoldState: ScaffoldState, navController: NavController) {
-    val items = listOf(
-        NavDrawerItem.Home,
-        NavDrawerItem.Music,
-        NavDrawerItem.Movies,
-        NavDrawerItem.Books,
-        NavDrawerItem.Profile,
-        NavDrawerItem.Settings
     )
-    Column(
-        modifier = Modifier
 
-    ) {
-        // Header
 
-        // Space between
-        Spacer(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(5.dp)
-        )
-        // List of navigation items
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
-        items.forEach { item ->
+    { contentPadding ->
+        Column() {
 
-            /* Add code later */
 
+            Text(
+                text = "Hello, " + name,
+
+                modifier = Modifier
+                    .padding(contentPadding)
+                    .fillMaxWidth(),
+                fontSize = 35.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "Your COPD Severity Level is:",
+                modifier = Modifier
+                    .padding(contentPadding)
+                    .fillMaxWidth(),
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "" + sever,
+                modifier = Modifier
+                    .padding(contentPadding)
+                    .fillMaxWidth(),
+                fontSize = 35.sp,
+                textAlign = TextAlign.Center
+            )
         }
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = "Developed by John Codeos",
-            color = Color.White,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(12.dp)
-                .align(Alignment.CenterHorizontally)
+
+
+// icons to mimic drawer destinations
+        val items = listOf(Icons.Default.Favorite, Icons.Default.Face, Icons.Default.Email)
+        val selectedItem = remember { mutableStateOf(items[0]) }
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Spacer(Modifier.height(12.dp))
+                    items.forEach { item ->
+                        NavigationDrawerItem(
+                            icon = { Icon(item, contentDescription = null) },
+                            label = { Text(item.name) },
+                            selected = item == selectedItem.value,
+                            onClick = {
+                                scope.launch { drawerState.close() }
+                                selectedItem.value = item
+                            },
+                            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                        )
+                    }
+                }
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = if (drawerState.isClosed) ">>> Swipe >>>" else "<<< Swipe <<<")
+                    Spacer(Modifier.height(20.dp))
+
+                }
+            }
         )
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
-@Composable
-fun DrawerPreview() {
-    val scope = rememberCoroutineScope()
-    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
-    val navController = rememberNavController()
-    Drawer(scope = scope, scaffoldState = scaffoldState, navController = navController)
-}
