85e4438ff7120b11f51c780ffa6718ec3247888e
Parth Jain <parthjain1812@gmail.com>
2023-08-13 21:42:05 +0530

    new location getter

--- COPDAsthma/app/src/main/java/com/example/copd_asthma/features/location/locationGetter.kt
+++ COPDAsthma/app/src/main/java/com/example/copd_asthma/features/location/locationGetter.kt
@@ -1,107 +1,42 @@
 package com.example.copd_asthma.features.location
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
+import android.widget.Toast
+import com.example.copd_asthma.fusedLocationClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+
 
 @SuppressLint("MissingPermission")
 fun GetLocation(context: Context, onLocationReceived: (Double, Double) -> Unit) {
-    val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-    val locationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-            locationManager.removeUpdates(this)
-            onLocationReceived(location.latitude, location.longitude)
+
+    fusedLocationClient = LocationServices.getFusedLocationProviderClient(context as Activity)
+
+    fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+        override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+        override fun isCancellationRequested() = false
+    })
+        .addOnSuccessListener { location: Location? ->
+            if (location == null)
+                Toast.makeText(context, "Cannot get location.", Toast.LENGTH_SHORT).show()
+            else {
+                val lat = location.latitude
+                val lon = location.longitude
+                onLocationReceived(location.latitude, location.longitude)
+            }
         }
-    }
-
-
-
-    locationManager.requestSingleUpdate(
-        LocationManager.GPS_PROVIDER,
-        locationListener,
-        null)
 
 
 
 
-
-
-
-    locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, locationListener, null)
 }
 
 
 
-//@SuppressLint("MissingPermission")
-//@Composable
-//fun getCurrentLocation(): Pair<Double, Double>? {
-//
-//    val context = LocalContext.current
-//    val fusedLocationClient: FusedLocationProviderClient =
-//        LocationServices.getFusedLocationProviderClient(context)
-//
-//    var currLocation by remember { mutableStateOf<Pair<Double, Double>?>(null) }
-//    fusedLocationClient.lastLocation
-//        .addOnSuccessListener { location : Location? ->
-//            if (location != null) {
-//                currLocation = Pair(location.latitude, location.longitude)
-//            }
-//        }
-//
-//
-//
-//    val requestPermissionLauncher = rememberLauncherForActivityResult(
-//        contract = ActivityResultContracts.RequestPermission()
-//    ) { isGranted ->
-//        if (isGranted) {
-//            startLocationUpdates(fusedLocationClient) { location ->
-//                currLocation = location
-//            }
-//        } else {
-//            currLocation = null
-//        }
-//    }
-//
-//    LaunchedEffect(Unit) {
-//        val locationPermission = Manifest.permission.ACCESS_FINE_LOCATION
-//        val hasLocationPermission = ContextCompat.checkSelfPermission(context, locationPermission)
-//        if (hasLocationPermission == PackageManager.PERMISSION_GRANTED) {
-//            startLocationUpdates(fusedLocationClient) { location ->
-//                currLocation = location
-//            }
-//        } else {
-//            requestPermissionLauncher.launch(locationPermission)
-//        }
-//    }
-//
-//    return currLocation
-//}
-//@SuppressLint("MissingPermission")
-//fun startLocationUpdates(fusedLocationClient: FusedLocationProviderClient, onLocationUpdate: (Pair<Double, Double>?) -> Unit ) {
-//
-//    val locationCallback = object : LocationCallback() {
-//            override fun onLocationResult(locationResult: LocationResult) {
-//                val latestLocation: Location? = locationResult.lastLocation
-//                latestLocation?.let { location ->
-//                    val latitude = location.latitude
-//                    val longitude = location.longitude
-//                    onLocationUpdate(Pair(latitude, longitude))
-//                }
-//            }
-//        }
-//
-//
-//        val locationRequest = LocationRequest.create().apply {
-//            priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
-//            interval = (60_000*3)
-//        }
-//
-//        fusedLocationClient.requestLocationUpdates(
-//            locationRequest,
-//            locationCallback,
-//            Looper.getMainLooper()
-//        )
-//
-//}
